81df7580a460175d4131c51893070c681bf565d4
HenriqueAPMendes <hapmendesss@gmail.com>
2023-12-14 17:22:32 -0400

    lazy not finished

--- cses/RangeQueries/1651_RangeUpdateQueries.cpp
+++ cses/RangeQueries/1651_RangeUpdateQueries.cpp
@@ -1,86 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-#define left(p) (2*p+1)
-#define right(p) (2*p+2)
-
 #define ll long long
 
-#define MAX 212345
-int n;
-vector<ll> st(4*MAX, 0), lz(4*MAX, 0), v(MAX, 0);
+const int MAX = 212345;
 
-void build(int p, int l, int r){
+vector<ll> a(MAX), tree(4*MAX), lazy(4*MAX);
 
-    if(l==r) { st[p] = v[l]; return; }
-    int m = (l+r)>>1;
+void build(int node, int start, int end){
+    if (start == end) tree[node] = a[start];
+    else{
+        int mid = (start+end)>>1;
 
-    build(left(p), l, m);
-    build(right(p), m+1, r);
+        build(2*node,start,mid);
+        build(2*node+1,mid+1,end);
 
-    st[p] = st[left(p)] + st[right(p)];
-}
-
-void unlazy(int p, int tl, int tr){
-    if(lz[p] == 0) return;
-    st[p] = lz[p];
-    if(tl != tr){
-        lz[left(p)] = lz[p];
-        lz[right(p)] = lz[p];
+        tree[node] = tree[2*node] + tree[2*node+1];
     }
-    
-    lz[p] = 0;
 }
 
+void updateRange(int node, int start, int end, int l, int r, int val){
+    if (lazy[node]){
+        tree[node] += (end-start+1) * lazy[node];
+        if (start != end){
+            lazy[2*node] += lazy[node];
+            lazy[2*node+1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
 
-void update(int p, int tl, int tr, int l, int r, int v){
-    unlazy(p, tl, tr);
-    
-    if(tl > r || tr < l) return;
+    if (start>end || start>r || end<l) return;
 
-    if(tl >= l && tr <= r){
-        lz[p] = v;
-        unlazy(p, tl, tr);
+    if (start>=l && end<=r){
+        tree[node] += (end-start+1) * val;
+        if (start != end){
+            lazy[2*node] += val;
+            lazy[2*node+1] += val;
+        }
         return;
     }
 
-    int m = (tl+tr)>>1;
-    update(left(p), tl, m, l, r ,v);
-    update(right(p), m+1, tr, l, r, v);
-    
-    st[p] = (st[left(p)] + st[right(p)]);
+    int mid = (start+end) >> 1;
+    updateRange(2*node, start, mid, l, r, val);
+    updateRange(2*node+1, mid+1, end, l, r, val);
+    tree[node] = tree[2*node] + tree[2*node+1];
 }
 
+ll queryRange(int node, int start, int end, int l, int r){
+    if (start>end || start>r || end<l) return 0;
 
-int query(int p, int l, int r, int i){
-    unlazy(p, l, r);
-    
-    int m = (l+r)>>1;
-    if (m == i && l == r) return st[p];
-    if (m < i) return query(left(p), l, m, i);
-    else return query(right(p), m+1, r, i);
+    if (lazy[node]){
+        tree[node] += (end-start+1) * lazy[node];
+        if (start != end){
+            lazy[2*node] += lazy[node];
+            lazy[2*node+1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+
+    if (start>=l && end<=r) return tree[node];
+
+    int mid = (start+end)>>1;
+    ll p1 = queryRange(2*node, start, mid, l, r);
+    ll p2 = queryRange(2*node+1, mid+1, end, l, r);
+    return (p1+p2);
 }
 
-int main(){
-    cin.tie(0);
+int main (){
+    cin.tie(0); cout.tie(0);
     ios_base::sync_with_stdio(0);
 
-    int n, q, l, r, op, x;
+    int n, q;
     cin >> n >> q;
-    for (int i = 0; i < n; i++) cin >> v[i];
-    build(0, 0, n-1);
+    for (int i = 0; i < n; i++) cin >> a[i];
 
+    for (int i = 0; i < n; i++) cout << a[i] << ' ';
+    cout << endl;
+
+    build(1,0,n-1);
+    
+    int a,b,k,u,op;
     while(q--){
         cin >> op;
         if (op == 1){
-            cin >> l >> r >> x;
-            update(0, 0, n-1, l, r, x);
+            cin >> a >> b >> u;
+            updateRange(1,0,n-1,a,b,u);
         }
         else{
-            cin >> x;
-            cout << query(0, 0, n-1, x);
-        }        
+            cin >> k;
+            cout << queryRange(1,0,n-1,k,k) << '\n';
+        }
     }
 
     return 0;
