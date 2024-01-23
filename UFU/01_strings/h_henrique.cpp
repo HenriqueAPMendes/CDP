4091e1f73036be931658e622984ba34b222541a3
HenriqueAPMendes <hapmendesss@gmail.com>
2024-01-22 22:30:39 -0300

    wcin wstring wcout

--- UFU/01_strings/h_henrique.cpp
+++ UFU/01_strings/h_henrique.cpp
@@ -1,20 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isUpper(char c){
+    return (c >= 'A' and c <= 'Z');
+}
+
+bool isLower(char c){
+    return (c >= 'a' and c <= 'z');
+}
+
+bool isLetter(char c){
+    return (isUpper(c) or isLower(c));
+}
+
 int main(){  
-    setlocale(LC_ALL, "portuguese-brazilian");
-    string s; getline(cin,s);
-    int t; cin >> t;
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    wstring s; getline(wcin, s);
+    wcout << s << endl;
+    int t; wcin >> t;
     t %= 26;
 
     for (int i = 0; i < (int)s.length(); i++){
-        
-        if (s[i] == ' ') continue;
-        if (s[i] >= 'A' and s[i] <= 'Z' and s[i]+t > 'Z') s[i] = 'A'+26-t;
-        else if (s[i] >= 'a' and s[i] <= 'z' and s[i]+t > 'z') s[i] = 'a'+26-t;
-        else s[i] =  s[i]+t;
+        if (!isLetter(s[i])) continue;
+        if (isUpper(s[i]) and s[i]+t > 'Z') s[i] = 'A' + (t - 'Z' + s[i] - 1);
+        else if (isLower(s[i]) and s[i]+t > 'z') s[i] = 'a' + (t - 'z' + s[i] - 1);
+        else s[i]+=t;
     }
-    cout << s << endl;
+
+    wcout << s << endl;
 
     return 0;
 }
