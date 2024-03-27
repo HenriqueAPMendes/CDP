7722ef2d82887efbc898ce54d90aecfd51a38dd2
HenriqueAPMendes <hapmendesss@gmail.com>
2024-03-27 11:49:52 -0300

    2sat 1 passed, 2sat 2 not passed

--- CodeForces/div2/400/d.cpp
+++ CodeForces/div2/400/d.cpp
@@ -7,23 +7,15 @@ using namespace std;
 #define ii pair<int,int>
 #define vii vector<ii>
 
-vi vis, ts, scc;
+vi comp;
 vii switches;
-vector<vi> adj, adjT;
+vector<vi> adj;
 
-void dfs1(int u){
-    vis[u] = 1;
+void dfs(int u, int c){
+    comp[u] = c;
     for (auto v : adj[u])
-        if (!vis[v]) dfs1(v);
-    ts.push_back(u);
+        if (!comp[v]) dfs(v, c);
 }
-
-void dfs2(int u, int c){
-    scc[u] = c;
-    for (auto v : adjT[u])
-        if (!scc[v]) dfs2(v, c);
-}
-
 signed main(){
     cin.tie(0); cout.tie(0);
     ios_base::sync_with_stdio(0);
@@ -32,54 +24,47 @@ signed main(){
     cin >> n >> m;
     
     vi initial(n);
-    vis = scc = vi(n);
+    comp = vi(2*m);
     switches = vii(n, {-1,-1});
-    adj = adjT = vector<vi>(2*n);
+    adj = vector<vi>(2*m);
 
     for (int i = 0; i < n; i++) cin >> initial[i];
+    
     for (int i = 0; i < m; i++){
         int qtd; cin >> qtd;
-        // cout << "switches: ";
         while(qtd--){
             int s; cin >> s;
             s--;
-            // cout << s << ' ' << switches[s].first << ' ' << switches[s].second << endl;
             if (switches[s].first == -1) switches[s].first = i;
             else switches[s].second = i;
         }
-        // cout << endl;
-
     }
 
+    // construir grafo de implicacoes
     for (int i = 0; i < n; i++){
         int a = switches[i].first;   
         int b = switches[i].second;
-        // cout << initial[i] << ' ' << a << ' ' << b << endl;   
         if (initial[i] == 1){
             adj[a].push_back(b);
-            adjT[b+n].push_back(a+n);
+            adj[a+m].push_back(b+m);
+            adj[b].push_back(a);
+            adj[b+m].push_back(a+m);
         }
         else {
-            adj[a].push_back(b+n);
-            adjT[b].push_back(a+n);
+            adj[a].push_back(b+m);
+            adj[a+m].push_back(b);
+            adj[b+m].push_back(a);
+            adj[b].push_back(a+m);
         }
     }
 
-    for (int i = 0; i < n; i++)
-        if (!vis[i]) dfs1(i);
-
-    cout << "ts: ";
-    for (auto x : ts) cout << x << ' ';
-    cout << endl;
-
-    int c = 1;
-    for (int i = n-1; i >= 0; i--)
-        if (!scc[ts[i]]) dfs2(i, c++);
+    for (int i = 0; i < 2*m; i++)
+        if (!comp[i]) dfs(i, i+1);
 
     bool valid = true;
-    for (int i = 0; i < m; i++){
-        if (scc[i] != scc[i+n]) valid = false;
-    }
+    for (int i = 0; i < m; i++)
+        if (comp[i] == comp[i+m]) valid = false;
+    
 
     cout << (valid ? "YES" : "NO") << endl;
 
---
+++ cses/Graphs/GiantPizza-1684.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define endl '\n'
+#define ii pair<int,int>
+#define vii vector<ii>
+
+#define PLUS 1
+#define MINUS 2
+
+vector<vi> adj, adjT;
+vi comp, vis, ts;
+
+void dfs1(int u){
+    vis[u] = 1;
+    for (auto v : adj[u])
+        if (!vis[v]) dfs1(v);
+    ts.push_back(u);
+}
+
+void dfs2(int u, int c){
+    comp[u] = c;
+    for (auto v : adjT[u])
+        if (!comp[v]) dfs2(v, c);
+}
+
+void dfs3(int u, int c){
+    comp[u] = c;
+    for (auto v : adj[u])
+        if (!comp[v]) dfs2(v, c);
+}
+
+int main(){
+    cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int n, m;
+    cin >> n >> m;
+    adj = adjT = vector<vi>(2*m);
+    comp = vis = vi(2*m);
+
+    for (int i = 0; i < n; i++){
+        char s1, s2;
+        int t1, t2;
+        cin >> s1 >> t1 >> s2 >> t2;
+        t1--; t2--;
+        if (s1 == '+' && s2 == '+'){
+            adj[t1+m].push_back(t2);
+            adj[t2+m].push_back(t1);
+            adjT[t2].push_back(t1+m);
+            adjT[t1].push_back(t2+m);
+        }
+        else if (s1 == '-' && s2 == '+'){
+            adj[t1].push_back(t2);
+            adj[t2+m].push_back(t1+m);
+            adjT[t2].push_back(t1);
+            adjT[t1+m].push_back(t2+m);
+        }
+        else if (s1 == '+' && s2 == '-'){
+            adj[t1+m].push_back(t2+m);
+            adj[t2].push_back(t1);
+            adjT[t1].push_back(t2);
+            adjT[t2+m].push_back(t1+m);
+        }
+        else { // - -
+            adj[t1].push_back(t2+m);
+            adj[t2].push_back(t1+m);
+            adjT[t2+m].push_back(t1);
+            adjT[t1+m].push_back(t2);
+        }
+    }
+
+    for (int i = 0; i < 2*m; i++)
+        if (!vis[i]) dfs1(i);
+
+    reverse(ts.begin(), ts.end());
+    for (auto u : ts)
+        if (!comp[u]) dfs2(u, u+1);
+    
+    bool ok = true;
+    for (int i = 0; i < m; i++)
+        if (comp[i] == comp[i+m]) ok = false;
+    
+    if (!ok) { cout << "IMPOSSIBLE" << endl; return 0; }
+
+    // atribuir valores atraves do grafo
+    for (auto v : ts){
+        
+    }
+
+    for (int i = 0; i < m; i++){
+        if (comp[i] == PLUS) cout << "+ ";
+        else cout << "- ";
+    }
+    cout << endl;
+
+    return 0;
+}
